e1a61e6d25af6091052ee16f1b7b0ada7c47301f
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-05-21 22:05:30 +0700

    input valid

--- app/src/main/java/id/wikosac/storyapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/id/wikosac/storyapp/ui/auth/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.util.Patterns
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
@@ -42,19 +43,49 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        setMyButtonEnable()
         edEmail.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                setMyButtonEnable()
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable) {
+                val email = s.toString()
+                if (!isEmailValid(email)) {
+                    edEmail.error = "Invalid email address"
+                } else {
+                    edEmail.error = null
+                }
             }
-            override fun afterTextChanged(s: Editable) {}
+        })
+
+        edPass.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val pass = s.toString()
+                if (!isPassValid(pass)) {
+                    edPass.error = "Password must be greater than or equal to 8"
+                } else {
+                    edPass.error = null
+                    setMyButtonEnable()
+                }
+            }
         })
     }
 
     private fun setMyButtonEnable() {
-        val email = binding.edLoginEmail.text
-        val pass = binding.edLoginPassword.text
-        binding.btnLogin.isEnabled = (email != null && pass != null) && (email.toString().isNotEmpty() && email.toString().isNotEmpty())
+        binding.btnLogin.isEnabled = edPass.text.toString().isNotEmpty()
     }
+
+    private fun isEmailValid(email: String): Boolean {
+        val pattern = Patterns.EMAIL_ADDRESS
+        return pattern.matcher(email).matches()
+    }
+
+    private fun isPassValid(pass: String): Boolean {
+        return pass.length >= 8
+    }
+
 }
